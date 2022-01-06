adbac7508f20b668cca77639fba28d7d14e5d373
Kevin Boos <kevinaboos@gmail.com>
2022-01-05 18:06:22 -0800

    Add basic memory mapping functions

--- src/theseus/mod.rs
+++ src/theseus/mod.rs
@@ -27,6 +27,9 @@ pub type c_long = i64;
 pub type c_ulong = u64;
 pub type off_t = i64;
 
+pub type wint_t = u32;
+
+
 cfg_if! {
     if #[cfg(target_arch = "x86_64")] {
         mod x86_64;
@@ -59,37 +62,72 @@ if #[cfg(libc_core_cvoid)] {
 
 
 
-pub const PROT_NONE: c_int = 0;
-pub const PROT_READ: c_int = 1;
-pub const PROT_WRITE: c_int = 2;
-pub const PROT_EXEC: c_int = 4;
-
-
-pub const MAP_FILE: c_int = 0x0000;
-pub const MAP_SHARED: c_int = 0x0001;
-pub const MAP_PRIVATE: c_int = 0x0002;
-pub const MAP_ANON: c_int = 0x0020;
-pub const MAP_ANONYMOUS: c_int = MAP_ANON;
-pub const MAP_FIXED: c_int = 0x0010;
-pub const MAP_FAILED: *mut c_void = !0 as _;
-
-pub const MS_ASYNC: c_int = 0x0001;
-pub const MS_INVALIDATE: c_int = 0x0002;
-pub const MS_SYNC: c_int = 0x0004;
-
-
-extern "C" {
-    pub fn mlock(addr: *const c_void, len: size_t) -> c_int;
-    pub fn munlock(addr: *const c_void, len: size_t) -> c_int;
-    pub fn mlockall(flags: c_int) -> c_int;
-    pub fn munlockall() -> c_int;
-    pub fn mmap(
-        addr: *mut c_void,
-        len: size_t,
-        prot: c_int,
-        flags: c_int,
-        fd: c_int,
-        offset: off_t,
-    ) -> *mut c_void;
-    pub fn munmap(addr: *mut c_void, len: size_t) -> c_int;
+#[cfg_attr(feature = "extra_traits", derive(Debug))]
+pub enum FILE {}
+impl Copy for FILE {}
+impl Clone for FILE {
+    fn clone(&self) -> FILE {
+        *self
+    }
 }
+#[cfg_attr(feature = "extra_traits", derive(Debug))]
+pub enum fpos_t {} 
+impl Copy for fpos_t {}
+impl Clone for fpos_t {
+    fn clone(&self) -> fpos_t {
+        *self
+    }
+}
+
+
+mod mm {
+    use super::*;
+
+    pub const PROT_NONE: c_int = 0;
+    pub const PROT_READ: c_int = 1;
+    pub const PROT_WRITE: c_int = 2;
+    pub const PROT_EXEC: c_int = 4;
+
+    pub const MAP_FILE: c_int = 0x0000;
+    pub const MAP_SHARED: c_int = 0x0001;
+    pub const MAP_PRIVATE: c_int = 0x0002;
+    pub const MAP_ANON: c_int = 0x0020;
+    pub const MAP_ANONYMOUS: c_int = MAP_ANON;
+    pub const MAP_FIXED: c_int = 0x0010;
+    pub const MAP_FAILED: *mut c_void = !0 as _;
+
+    pub const MS_ASYNC: c_int = 0x0001;
+    pub const MS_INVALIDATE: c_int = 0x0002;
+    pub const MS_SYNC: c_int = 0x0004;
+
+    extern "C" {
+        pub fn mlock(addr: *const c_void, len: size_t) -> c_int;
+        pub fn munlock(addr: *const c_void, len: size_t) -> c_int;
+        pub fn mlockall(flags: c_int) -> c_int;
+        pub fn munlockall() -> c_int;
+        pub fn mmap(
+            addr: *mut c_void,
+            len: size_t,
+            prot: c_int,
+            flags: c_int,
+            fd: c_int,
+            offset: off_t,
+        ) -> *mut c_void;
+        pub fn munmap(addr: *mut c_void, len: size_t) -> c_int;
+    }
+}
+pub use self::mm::*;
+
+
+mod printf {
+    use super::*;
+    
+    extern "C" {
+        // `FILE` not yet supported
+        // pub fn fprintf(stream: *mut FILE, format: *const c_char, ...) -> c_int;
+        pub fn printf(format: *const c_char, ...) -> c_int;
+        pub fn snprintf(s: *mut c_char, n: size_t, format: *const c_char, ...) -> c_int;
+        pub fn sprintf(s: *mut c_char, format: *const c_char, ...) -> c_int;
+    }
+}
+pub use self::printf::*;
